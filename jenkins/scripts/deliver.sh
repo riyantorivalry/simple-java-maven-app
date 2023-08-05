8bc104a03310b2929b97c1b0c495d7cd3e5cb36b
Riyanto <riyanto.rivalry@gmail.com>
2023-08-06 00:00:55 +0700

    update pipeline

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,3 +24,6 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
+sleep 60
+echo jps | grep "war" | awk '{print $1}' > .pidfile
+set +x
