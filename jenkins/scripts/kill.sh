08ba6dc0d5eedde42f306c22f10397e8dd4d4972
Riyanto <riyanto.rivalry@gmail.com>
2023-08-06 09:35:19 +0700

    fix permission denied kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -4,4 +4,6 @@ echo 'The following command terminates the "JVM container" process using its PID
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
 set -x
+chmod +x ./jenkins/scripts/kill.sh
 kill $(cat .pidfile)
+set x
